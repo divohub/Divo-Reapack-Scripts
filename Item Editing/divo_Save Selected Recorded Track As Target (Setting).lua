e8d0cd009f9ac9b0091c7654bd46e4e15e3ffc98
Fedor Kupryakov <hihi@divosound.com>
2025-04-25 09:03:20 +0300

    change language

--- Item Editing/divo_Save Selected Recorded Track As Target (Setting).lua
+++ Item Editing/divo_Save Selected Recorded Track As Target (Setting).lua
@@ -1,6 +1,6 @@
 -- @description A script for selecting a target rec track for a project (works with Recorded Media Item On Selected Track) 
 -- @author divohub
--- @version 1.0
+-- @version 1.1
 
 
 
@@ -18,42 +18,22 @@ function SaveSelectedTrackGUID()
     local track = reaper.GetSelectedTrack(0, 0)
 
     if not track then
-        reaper.ShowMessageBox("Ошибка не выбран трек","Ошибка", 0)
+        reaper.ShowMessageBox("Track is not selected","Ошибка", 0)
         return
     end
 
     local guid = reaper.GetTrackGUID(track)
     if not guid or guid == "" then 
-        reaper.ShowMessageBox("Guid не получен", "Ошибка", 0)
+        reaper.ShowMessageBox("No GUID", "Ошибка", 0)
         return
     end
     
     reaper.SetProjExtState(0, section, key, guid)
     local _, rec_guid = reaper.GetProjExtState(0, section, key)
     if rec_guid == guid then
-        reaper.ShowMessageBox("GUID успешно сохранён", "Готово", 0)
+        reaper.ShowMessageBox("Selected Track is now Target Rec Track", "Готово", 0)
     end
 
-end
-
-function GetTrackbySavedGUID()
-    local _, rec_guid = reaper.GetProjExtState(0, section, key)
-    if not rec_guid or rec_guid == "" then
-        reaper.ShowMessageBox("Не найден сохраненный трек, пожалуйста, сохарните трек с помощью скрипта настройки", "Ошибка", 0)
-        return nil
-    end
-    local track_count = reaper.CountTracks(0)
-
-    for i = 0, track_count - 1 do 
-        local track = reaper.GetTrack(0, i)
-        local guid = reaper.GetTrackGUID(track)
-        if guid == rec_guid then 
-            return track
-        end
-    end
-    return nil
-
-    
 end
 
 SaveSelectedTrackGUID()
